210ad1418dbec46a553346c2fd2378dbce3ce074
CandyceSarringar <sarringarcd@hendrix.edu>
2015-10-09 10:56:33 -0500

    Rules

--- src/game/Rules.java
+++ src/game/Rules.java
@@ -3,4 +3,31 @@ package game;
 public class Rules {
 	
 	//for getting legal moves
+	
+	/*Rules:
+	 * 
+	 * Black moves first, then players alternate turns.
+	 * 
+	 * Non-king pieces can only move forwards.
+	 * 
+	 * Pieces not jumping can only move forward one square at a time (I think this
+	 * may have already been covered by the prev. piece movement).
+	 * 
+	 * Only one opponent piece can be captured per jump, but multiple jumps are 
+	 * allowed.
+	 * 
+	 * Pieces that are captured need to be removed from the board.
+	 * 
+	 * If a jump is available, it must be made. If more than one jump is available,
+	 * the player can choose either.
+	 * 
+	 * When a piece reaches the other side, it gets made into a king.
+	 * 
+	 * Kings can move backwards and forwards (Maybe have a king interface for this?)
+	 * 
+	 * Kings can do multiple jumps backwards and forwards, or a combination of those.
+	 * 
+	 * A player wins when the other cannot make a move.
+	 * 
+	 */
 }
