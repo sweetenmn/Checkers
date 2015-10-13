8cba134b1d6c028b85eedfd6888b148ec3cce57f
CandyceSarringar <sarringarcd@hendrix.edu>
2015-10-13 18:00:37 -0500

    BoardTest 2

--- src/checkersGUI/BoardTest.java
+++ src/checkersGUI/BoardTest.java
@@ -1,7 +1,9 @@
 package checkersGUI;
 
 import static org.junit.Assert.*;
+import game.Player;
 import helpers.GameState;
+import helpers.PlayerID;
 import javafx.scene.layout.GridPane;
 
 import org.junit.Test;
@@ -9,15 +11,19 @@ import org.junit.Test;
 public class BoardTest {
 	private GridPane grid;
 	Board board = new Board(grid, "name");
-
+	Player thisplayer;
 	//fails because of initialization in Board of Cell lastpiececlicked
 	@Test
 	public void PlayerTurn() {
+		board.createPlayer(PlayerID.BLACK);
 		assertTrue(board.getGameState() == GameState.BLACK_TURN);
+		assertTrue(board.isTurn(thisplayer));
 		board.swapPlayerTurn();
 		assertTrue(board.getGameState() == GameState.RED_TURN);
+		assertFalse(board.isTurn(thisplayer));
 		board.swapPlayerTurn();
 		assertTrue(board.getGameState() == GameState.BLACK_TURN);
+		assertTrue(board.isTurn(thisplayer));
 	}
 
 }
