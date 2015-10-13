7d137988b4f4c86ed58eb596ba040d4c6b0f4493
vXenonv <devin.henderson@rocketmail.com>
2015-10-12 21:29:07 -0500

    Changed Popup to an Alert

--- src/checkersGUI/Controller.java
+++ src/checkersGUI/Controller.java
@@ -1,100 +1,209 @@
 package checkersGUI;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import server_connection.MessageHandler;
+import server_connection.Server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.concurrent.ArrayBlockingQueue;
+
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
-import javafx.stage.Popup;
+import javafx.stage.*;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
 public class Controller {
-	
+	@FXML
+	BorderPane canvas;
 	@FXML
 	GridPane checkerboard;
-	
 	@FXML
-	Label p1 = new Label();
-	@FXML
-	Label p2 = new Label();
-	@FXML
-	Label playerTurn = new Label();
-	
-	@FXML
-	Button newGame = new Button();
+	Button newGame;
 	@FXML
 	Button connect = new Button("Connect");
 	@FXML
+	TextField playerOneInput = new TextField();
+	@FXML
+	TextField playerTwoInput = new TextField();
+	@FXML
 	Button submitMove;
-	
-	@FXML
-	TextField host = new TextField("Enter Host");
-	@FXML
-	TextField port = new TextField("Enter Port Number");
-	@FXML
-	TextField p1Name = new TextField("Enter your name");
-	@FXML
-	TextField p2Name = new TextField("Enter your enemy's name");
-	
 	Board board;
+	@FXML
+	Label playerOneLabel;
+	@FXML
+	Label playerTwoLabel;
+	@FXML
+	Label playerTurn;
+	@FXML
+	TextField hostText = new TextField();
+	MessageHandler messageHandler;
+	ArrayBlockingQueue<String> messages = new ArrayBlockingQueue<>(20);
 	
 	@FXML
 	public void initialize(){
-		board = new Board(checkerboard);
-		board.setUp();
+		playerOneInput.setPromptText("Enter your name");
+		playerTwoInput.setPromptText("Enter opponent's name");
+		hostText.setPromptText("Enter IP Address");
 	}
-	@FXML
-	public void popUp(){
-		/*Pops up a series of textfields and "connect" button.
-		 * The user will input the ip address and then press the connect button.
-		 * The connect button's method should then hide the textfield and button from view and push the screen.
-		 * I think the getIP should be linked to the "connect" button.
-		*/
-		VBox popupVBox = new VBox();
-		popupVBox.getChildren().add(p1Name);
-		popupVBox.getChildren().add(p2Name);
-		popupVBox.getChildren().add(host);
-		popupVBox.getChildren().add(port);
-		popupVBox.getChildren().add(connect);
-		Popup popup = new Popup();
-        popup.getContent().addAll(popupVBox);
-        popup.show(checkerboard, 800, 150);
-        connect.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent t) {
-                 getIP();
-                 p1.setText(p1Name.getText());
-                 p2.setText(p2Name.getText());
-                 playerTurn.setText(p1.getText() + "'s Turn");
-                 popup.hide();    
-            }
-        });
-        	
-    }
-	@FXML
-	public void getIP(){
-		//Connected to the New Game button, or activated upon initialization.
-		//Will ask for the other user's IP Address or *MAYBE* ask if the player
-		//wants to keep playing with their partner.
-		//If they do, use previous IP Address. Else, add a new one.
+	
+	public void startMessaging(){
+		new Thread(() -> {
+			for (;;) {
+				try {
+					String msg = messages.take();
+					System.out.println(msg);
+					Platform.runLater(() -> {messageHandler.handleMessage(msg);});
+				} catch (Exception e) {
+					badNews(e.getMessage());
+				}
+				
+			}
+		}).start();
 		
 	}
 	
 	@FXML
-	public void submove(){
-		board.move();
+	public void requestFocus(){
+		canvas.requestFocus();		
+	}
+	
+	void badNews(String what) {
+		Alert badNum = new Alert(AlertType.ERROR);
+		badNum.setContentText(what);
+		badNum.show();
+	}
+	
+	void handlePress(KeyCode code){
+		if (code == KeyCode.ENTER){
+			sendmove();
+		}
+	}
+	
+	@FXML
+	public void popUp(){
+		Stage window = new Stage();
+
+        //Block events to other windows
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle("Connection Window");
+        window.setMinWidth(250);
+        
+        VBox layout = new VBox(3);
+        layout.getChildren().addAll(playerOneInput, playerTwoInput, hostText, connect);
+        layout.setAlignment(Pos.CENTER);
+
+        //Display window and wait for it to be closed before returning
+        Scene scene = new Scene(layout);
+        window.setScene(scene);
+        window.showAndWait();
+        connect.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent t) {
+            	playerTurn.setText(playerOneInput.getText() + "'s Turn");
+            	submitMove.setOnAction(event -> sendmove());
+            	canvas.setOnKeyPressed(k -> handlePress(k.getCode()));               
+            }
+        });
+	}
+	private void startGame(){
+        requestFocus();
+        submitMove.setOnAction(event -> sendmove());
+        canvas.setOnKeyPressed(k -> handlePress(k.getCode()));
+
+		board = new Board(checkerboard);
+		board.setUp();
+		messageHandler = new MessageHandler(board);
+		requestFocus();
+		startMessaging();
+		createServer(hostText.getText());
+		
+		sendTo(hostText.getText(), 8888, 
+				messageHandler.generateSetUpMessage(playerOneInput.getText(), playerTwoInput.getText()));
+        playerOneLabel.setText(messageHandler.getPlayerOneName());
+        playerTwoLabel.setText(messageHandler.getPlayerTwoName());
+        playerTurn.setText(playerTwoLabel.getText() + "'s Turn");
+		
+	}
+
+	private void createServer(String host){
+		new Thread(() -> {
+            try {
+                Server s = new Server(8888, messageHandler, host);
+                s.listen();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+		}).start();
+	}
+
+	
+	void sendmove() {
+		try {
+			sendTo(hostText.getText(), 8888, messageHandler.getMovementMessage());
+			turnUpdate();
+		} catch (NumberFormatException nfe) {
+			badNews(this.hostText.getText() + " is not a valid IP Address");
+		
+		}
+	}
+	
+	void sendTo(String host, int port, String message) {
+		new Thread(() -> {
+			try {
+				Socket target = new Socket(host, port);
+				send(target, message);
+				receive(target);
+				target.close();
+			} catch (Exception e) {
+				Platform.runLater(() -> badNews(e.getMessage()));
+			}
+		}).start();
+	}
+	void send(Socket target, String message) throws IOException {
+		PrintWriter sockout = new PrintWriter(target.getOutputStream());
+		sockout.println(message);
+		sockout.flush();
+	}
+	
+	void receive(Socket target) throws IOException {
+		BufferedReader sockin = new BufferedReader(new InputStreamReader(target.getInputStream()));
+		while (!sockin.ready()) {}
+		while (sockin.ready()) {
+			try {
+				String msg = sockin.readLine();
+				messages.put(msg);
+			} catch (Exception e) {
+				Platform.runLater(() -> badNews(e.getMessage()));
+				e.printStackTrace();
+			}
+		}		
 	}
 	
 	@FXML
 	public void turnUpdate(){
 		/*This will update the Label displaying the current player's turn.
-		 * Can be used/used in conjuction with another method to allow the next player the ability to move their pieces
+		 * Can be used/used in conjunction with another method to allow the next player the ability to move their pieces
 		 * while restricting the other player.
 		*/
-		if(playerTurn.getText() == p1.getText() + "'s Turn")
-			playerTurn.setText(p2.getText() + "'s Turn");
+		if(playerTurn.getText() == playerOneLabel.getText() + "'s Turn")
+			playerTurn.setText(playerTwoLabel.getText() + "'s Turn");
 		else
-			playerTurn.setText(p1.getText() + "'s Turn");
+			playerTurn.setText(playerOneLabel.getText() + "'s Turn");
 	}
+
 }
