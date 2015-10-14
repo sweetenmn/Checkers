974fc518e6cdc63cb98fbb10bc20f7ce824a0996
Marina <sweet.rinaaa@gmail.com>
2015-10-14 07:45:31 -0500

    Moved test to right package

---
+++ src/helpers/TurnCounterTest.java
@@ -0,0 +1,20 @@
+package helpers;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TurnCounterTest {
+	TurnCounter turn = new TurnCounter();
+	
+	
+	@Test
+	public void test() {
+		assertTrue(turn.getCount() == 0);
+		turn.increment();
+		turn.increment();
+		turn.increment();
+		assertEquals(turn.getCount(), 3);
+	}
+
+}
