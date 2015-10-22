94a0958b3f1c4284fd57c7c6b3180a17b59077e4
Marina <sweet.rinaaa@gmail.com>
2015-10-22 17:58:23 -0500

    Fixed CellState issues
    actually able to test stuff now

--- src/game/CellTest.java
+++ src/game/CellTest.java
@@ -13,17 +13,57 @@ import checkersGUI.Board;
 public class CellTest {
 	private GridPane grid;
 	Board board = new Board(grid, "name");
-	Coordinate coord = new Coordinate(1,1);
-	Coordinate coord2 = new Coordinate(1,0);
 	TurnCounter counter = new TurnCounter();
+
+	@Test
+	public void stateTest(){
+		redCheck();
+		blackCheck();
+		emptyCheck();
+	}
 	
-	Cell cell = new Cell(board, CellState.BLACK, coord);
-	Cell cell2 = new Cell(board, CellState.BLACK, coord2);
+	public void redCheck(){
+		Coordinate redCoord = new Coordinate(0, 1);
+		Cell redCell = new Cell(board, CellState.RED, redCoord);
+		assertTrue(redCell.isRedChip());
+		assertFalse(redCell.isRedKing());
+		assertFalse(redCell.isEmpty());
+	}
+	
+	public void blackCheck(){
+		Coordinate blackCoord = new Coordinate(1, 7);
+		Cell blackCell = new Cell(board, CellState.BLACK, blackCoord);
+		assertTrue(blackCell.isBlackChip());
+		assertFalse(blackCell.isBlackKing());
+	}
+	
+	public void emptyCheck(){
+		Coordinate emptyCoord = new Coordinate(0, 5);
+		Cell emptyCell = new Cell(board, CellState.EMPTY, emptyCoord);
+		assertTrue(emptyCell.isEmpty());
+		
+	}
 	
 	@Test
-	public void test() {
-		assertTrue(cell.getState() != CellState.BLACK_KING);
-		assertTrue(cell2.getState() == CellState.BLACK_KING);
+	public void getCoordTest(){
+		Coordinate testCoord = new Coordinate(6, 2);
+		Cell blackCell = new Cell(board, CellState.BLACK, testCoord);
+		assertEquals(6, blackCell.getColumn());
+		assertEquals(2, blackCell.getRow());
+		assertEquals(testCoord, blackCell.getCoords());
+		assertTrue(blackCell.hasCoords(testCoord.column(), testCoord.row()));
+	}
+	
+	@Test
+	public void kingTest() {
+		Coordinate topRow = new Coordinate(2, 0);
+		Coordinate bottomRow = new Coordinate(1, 7);
+		Cell blackKing = new Cell(board, CellState.BLACK, topRow);
+		Cell redKing = new Cell(board, CellState.RED, bottomRow);
+		assertTrue(blackKing.isBlackKing());
+		assertFalse(blackKing.isBlackChip());
+		assertTrue(redKing.isRedKing());
+		assertFalse(redKing.isRedChip());
 	}
 
 }
