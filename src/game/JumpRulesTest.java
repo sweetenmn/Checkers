84113d470538157a0356329f8fd505841b700b01
CandyceSarringar <sarringarcd@hendrix.edu>
2015-10-23 08:29:34 -0500

    More tests

--- src/game/JumpRulesTest.java
+++ src/game/JumpRulesTest.java
@@ -1,7 +1,11 @@
 package game;
 
 import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
 import javafx.scene.layout.GridPane;
+import helpers.CellState;
 import helpers.Coordinate;
 import helpers.PlayerID;
 import helpers.TurnCounter;
@@ -17,30 +21,44 @@ public class JumpRulesTest {
 	JumpRules jump = new JumpRules(board, rules);
 	TurnCounter count = new TurnCounter();
 	Player player = new Player(PlayerID.BLACK, count);
-	
+	Coordinate originCoord = new Coordinate(4, 2);
+	Coordinate upRightCoord = originCoord.upRightCoord();
+	Coordinate upLeftCoord = originCoord.upLeftCoord();
+	Coordinate downRightCoord = originCoord.downRightCoord();
+	Coordinate downLeftCoord = originCoord.downLeftCoord();
+	Cell origin = new Cell(board, CellState.BLACK, originCoord);
+	Cell upLeft = new Cell(board, CellState.RED, upLeftCoord);
+	Cell upUpLeft = new Cell(board, CellState.RED, upLeftCoord.upLeftCoord());
+	Cell downRight = new Cell(board, CellState.EMPTY, downRightCoord);
+	Cell upRight = new Cell(board, CellState.EMPTY, upRightCoord);
+	Cell straightLeft = new Cell(board, CellState.EMPTY, 
+			upLeftCoord.downLeftCoord());
+	Cell downStrLeft = new Cell(board, CellState.EMPTY, 
+			straightLeft.getCoords().downLeftCoord());
 	
 	
 	@Test
-	public void test() {
+	public void testBeginningJump() {
+		setUpBoard();
 		assertFalse(jump.playerCanJump(player));
 		for(int i = 0; i < board.cells.size() ; i ++){
 			assertFalse(jump.hasJump(board.cells.get(i)));
+			assertTrue(jump.getPossibleEnemies(board.cells.get(i)) == null);
 		}
 	}
 	
 	@Test
-	public void test2(){
+	public void testValidDestination(){
+		setUpBoard();
 		Coordinate coord = new Coordinate(11,5);
 		Coordinate coord2 = new Coordinate(5, 11);
-		for(int i = 0; i < board.cells.size(); i ++){
-			assertTrue(jump.getPossibleEnemies(board.cells.get(i)) == null);
-		}
 		assertFalse(jump.validDestination(coord));
 		assertFalse(jump.validDestination(coord2));
 	}
 		
 	@Test
-	public void test3(){
+	public void testInBounds(){
+		setUpBoard();
 		for(int i = 0; i < board.cells.size(); i ++){
 			Coordinate coord3 = new Coordinate((board.cells.get(i).getColumn() - 1), 
 					(board.cells.get(i).getRow() - 1));
@@ -59,10 +77,23 @@ public class JumpRulesTest {
 	
 	@Test
 	public void test4(){
+		setUpBoard();
 		for(int i = 0; i < board.cells.size(); i ++){
 			Coordinate coord4 = new Coordinate((board.cells.get(i).getColumn() - 2), 
 				(board.cells.get(i).getRow() - 2));
 			assertTrue(jump.isJump(board.cells.get(i), board.getCellAt(coord4)));
 		}
+	}
+	
+	public void setUpBoard(){
+		board.cells = new ArrayList<Cell>();
+		board.cells.clear();
+		board.cells.add(origin);
+		board.cells.add(upLeft);
+		board.cells.add(upUpLeft);
+		board.cells.add(downRight);
+		board.cells.add(upRight);
+		board.cells.add(straightLeft);
+		board.cells.add(downStrLeft);
 	}
 }
